59a580c0c9d502c89c3c03e98ff6a4f6afb82540
tanvir <tanvirulislam149@gmail.com>
2024-12-12 21:02:45 +0600

    template string

---
+++ template_string.js
@@ -0,0 +1,16 @@
+// Template string uses ``
+
+const name = "Shuvo";
+const age = 24;
+const my_text = `Hi, I am Shuvo`; // Template string
+const multiline_string = `Hello,   
+Welcome to revision codes. 
+Now learning template string`; // multiline string using template string
+const text = `My name is ${name}. I am ${age} years old.`; // Dynamic string
+const obj = { name: "Shuvo", age: 24 };
+const new_text = `It's ${obj.name}. My age is ${obj.age}`;
+
+console.log(my_text);
+console.log(multiline_string);
+console.log(text);
+console.log(new_text);
